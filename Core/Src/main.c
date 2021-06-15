8af93d0158db11a66991ea6643a0f4fb1240f89f
trteodor <trteodor@gmail.com>
2021-06-15 17:16:47 +0200

    StillTrying

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -121,6 +121,14 @@ int main(void)
 
   /* Initialize interrupts */
   MX_NVIC_Init();
+
+  static lv_disp_draw_buf_t disp_buf;
+
+  /*Static or global buffer(s). The second buffer is optional*/
+  static lv_color_t buf_1[240 * 10];
+  static lv_color_t buf_2[240 * 10];
+  static lv_disp_drv_t disp_drv;
+
   /* USER CODE BEGIN 2 */
 
 
@@ -133,16 +141,12 @@ int main(void)
 
   lv_init(); // LVGL
   /*A static or global variable to store the buffers*/
-  static lv_disp_draw_buf_t disp_buf;
 
-  /*Static or global buffer(s). The second buffer is optional*/
-  static lv_color_t buf_1[240 * 10];
-  static lv_color_t buf_2[240 * 10];
 
   /*Initialize `disp_buf` with the buffer(s). With only one buffer use NULL instead buf_2 */
   lv_disp_draw_buf_init(&disp_buf, buf_1, buf_2, 240 *10);
 
-  static lv_disp_drv_t disp_drv;          /*A variable to hold the drivers. Must be static or global.*/
+           /*A variable to hold the drivers. Must be static or global.*/
   lv_disp_drv_init(&disp_drv);            /*Basic initialization*/
   disp_drv.draw_buf = &disp_buf;          /*Set an initialized buffer*/
   disp_drv.flush_cb = ILI9341_flush;        /*Set a flush callback to draw to the display*/
--- ILI9341/lv_driver.c
+++ ILI9341/lv_driver.c
@@ -15,16 +15,21 @@ void ILI9341_flush(lv_disp_drv_t * drv, const lv_area_t * area,  lv_color_t * co
 		ILI9341_SendCommand(ILI9341_RAMWR);  //Ta komenda przed zapisem!!!
 	uint32_t size = (area->x2 - area->x1 + 1) * (area->y2 - area->y1 + 1);
 	//Send_DMA_Data16(&color_map->full, size);
+
 	uint16_t *Color;
 	Color=malloc(sizeof(uint16_t)); //memory !
 	*Color=0xF800;
 
 	Send_DMA_Data16(Color, size);
+	while(flag_DMA_Stream_bsy)
+	{
+	}
+	lv_disp_flush_ready(drv);
 }
 
 void DMA_ILI9341_SPI_TransmitComplete_Callback()
 {
-	lv_disp_flush_ready(LastDriver);
+
 }
 
 
